5068a8bf3509873d501c555345c7e4abd3c531dd
Vincent <vleclerc@inow.fr>
2019-07-08 15:18:33 +0200

    ajout material et menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pierrevacances';
+  title = 'Pierre & Vacances';
+  user: any = null;
 }
